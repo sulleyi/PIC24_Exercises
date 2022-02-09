759966fdb9bae6749b056e628ac8e8a93b961c24
Ian <ians@convertsation.org>
2022-02-09 10:27:02 -0500

    Setup Exercise 7

---
+++ ex7_ultrasonic.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ex7_ultrasonic.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ex7_ultrasonic.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ex7ultrasonic.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ex7ultrasonic.x.tar
