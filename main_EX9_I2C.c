3ae48ada4190a7b3dd485cc1fdb87716b133e14f
Ian <iansulley2@gmail.com>
2022-02-23 15:04:16 -0500

    setup serial and I2C communication

--- main_EX9_I2C.c
+++ main_EX9_I2C.c
@@ -9,31 +9,38 @@
 
 // #include for textbook library header files
 #include "pic24_all.h"
+#include "stdio.h"
 
-
+#define I2C_CLK_SPEED  100//in kHz
 /*********** GLOBAL VARIABLE AND FUNCTION DEFINITIONS *******/
-
 
     
 /********** MAIN PROGRAM ********************************/
 int main ( void )  //main function that....
 { 
 /* Declare local variables */
+uint8_t send_bits;
+uint8_t ack_bits;
 
+uint16_t temp;
 
 /* Call configuration routines */
 	configClock();  //Sets the clock to 40MHz using FRC and PLL
-	configI2C1(I2C_CLK_SPEED);
+	configI2C1(I2C_CLK_SPEED); //clock speed in kHZ
 	startI2C1();
-
+	configUART1(230400);
 
 /* Initialize ports and other one-time code */
+putI2C1(send_bits);
+temp = getI2C1(ack_bits);
+DELAY_MS(100);
 
-
-    
 /* Main program loop */
 	while (1) {	
-		
+		putI2C1(send_bits);
+		temp = getI2C1(ack_bits);
+
+		DELAY_MS(100);
 		}
 return 0;
 }
